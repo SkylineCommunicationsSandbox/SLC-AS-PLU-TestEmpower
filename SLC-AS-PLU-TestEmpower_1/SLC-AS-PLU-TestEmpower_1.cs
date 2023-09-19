dc0ecc5df2b39a0423d0f8ff5c6d515531efc436
Pieter Lust <pieter.lust@skyline.be>
2023-09-19 15:16:01 +0200

    a change

--- SLC-AS-PLU-TestEmpower_1/SLC-AS-PLU-TestEmpower_1.cs
+++ SLC-AS-PLU-TestEmpower_1/SLC-AS-PLU-TestEmpower_1.cs
@@ -68,7 +68,7 @@ namespace SLC_AS_PLU_TestEmpower_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello World!");
+			engine.GenerateInformation("Hello World 123!");
 		}
 	}
 }
